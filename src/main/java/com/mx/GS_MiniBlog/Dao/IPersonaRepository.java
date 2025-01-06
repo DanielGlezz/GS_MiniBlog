e1edec563a61b4bff3af486eff4db91d5bc46a6b
Daniel Gonzalez Arrevillaga <105679322+DanielGlezz@users.noreply.github.com>
2025-01-06 08:01:49 -0600

    Update repository

--- src/main/java/com/mx/GS_MiniBlog/Dao/IPersonaRepository.java
+++ src/main/java/com/mx/GS_MiniBlog/Dao/IPersonaRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mx.GS_MiniBlog.Models.Persona;
 
+@Repository
 public interface IPersonaRepository extends JpaRepository<Persona, Long> {
 
 }
