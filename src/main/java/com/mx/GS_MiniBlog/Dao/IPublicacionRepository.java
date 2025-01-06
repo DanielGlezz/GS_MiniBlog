9baecbd49d697ed496a73084851e1391cce21c4a
Daniel Gonzalez Arrevillaga <105679322+DanielGlezz@users.noreply.github.com>
2025-01-06 07:56:09 -0600

    Update repository

--- src/main/java/com/mx/GS_MiniBlog/Dao/IPublicacionRepository.java
+++ src/main/java/com/mx/GS_MiniBlog/Dao/IPublicacionRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mx.GS_MiniBlog.Models.Publicacion;
 
+@Repository
 public interface IPublicacionRepository extends JpaRepository<Publicacion, Long> {
 
 }
